a35b58298d0d0f9df13788ad48de41c2e946f508
Jonas Bo Grimsgaard <jonasbg@gmail.com>
2012-09-21 10:18:37 +0200

    User

---
+++ Tallknuserne/Tallknuserne/User.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tallknuserne //samme som package i Java
+{
+    public class User
+    {
+        private String username;
+        private String password;
+        private int role;
+        private int user_id;
+
+        public User(String username, String password, int rights, int user_id)
+        {
+            this.username = username;
+            this.password = password;
+            this.rights = rights;
+            this.user_id = user_id;
+        }
+
+        public string Username
+        {
+            get{
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public String Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        public int role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                role = value;
+            }
+        }
+
+        public int User_id
+        {
+            get
+            {
+                return user_id;
+            }
+            set
+            {
+                user_id = value;
+            }
+        }
+    }
+}
