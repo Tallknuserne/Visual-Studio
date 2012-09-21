c2daa893f6ff6d52f1649c352919b6ad1fd83f80
Andrzej Thingstad <andrzey.thingstad@gmail.com>
2012-09-21 10:21:05 +0200

    Har laget en user, student og teacher klasse.

---
+++ Tallknuserne/Tallknuserne/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tallknuserne
+{
+    public class Teacher : User
+    {
+        public Teacher(String username, String password, int rights, int user_id)
+            : base(username, password, rights, user_id)
+        {
+        }
+    }
+    
+    public class Student : User
+    {
+        public Student(String username, String password, int rights, int user_id)
+            : base(username, password, rights, user_id)
+        {
+        }
+    }
+}
